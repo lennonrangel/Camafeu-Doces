aeec4d1eca7d77fdba7c8d5115c0f90aadf58069
lennonrangel <lennonpereirarangel@hotmail.com>
2025-04-01 18:44:11 -0300

    Criado menu de navegação

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import "./style.css";
+import logo from "../../assets/logo.png";
+import cook from "../../assets/cook.png";
 
 function Home() {
   return (
 
 <body>
-    
+      <header className="menu">
+        <nav>
+          <img src={logo} class="logo" alt="Logo" />
+          <ul className="list-menu">
+            <li><a href="home">Home</a></li>
+            <li><a href="produtos">Produtos</a></li>
+            <li><a href="sobre-nos">Empresa</a></li>
+            <li><a href="cardapio">Unidades</a></li>
+            <li><a href="contato">Contato</a></li>
+          </ul>
+        </nav>
+      </header>      
 </body>
   )
 }
